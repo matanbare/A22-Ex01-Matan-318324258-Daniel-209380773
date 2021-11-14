da0f30dcdd2f682fd152e4192ba86780859f02c2
matan <matanbare25@gmail.com>
2021-11-14 11:56:54 +0200

    Add project files.

---
+++ A22_Ex01_01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace A22_Ex01_01
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
